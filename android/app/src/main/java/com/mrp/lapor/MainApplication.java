f588b57056101a8bbe325c32c0ca86dde7de77e5
Made Raditya <radityapujamurti@gmail.com>
2018-08-05 14:21:13 +0800

    adding rn-elements

--- android/app/src/main/java/com/mrp/lapor/MainApplication.java
+++ android/app/src/main/java/com/mrp/lapor/MainApplication.java
@@ -4,6 +4,10 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -44,6 +48,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.asList(
         new MainReactPackage(),
+            new VectorIconsPackage(),
+            new VectorIconsPackage(),
+            new RNFirebasePackage(),
+            new VectorIconsPackage(),
         new RNFirebasePackage(),
         // add/remove these packages as appropriate
         new RNFirebaseAdMobPackage(),
